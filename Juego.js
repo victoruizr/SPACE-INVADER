715d4e9dd5cf172243ae54d0c3e7fcb626dabeb6
José Carlos Vargas Díaz <web.josecarlos@gmail.com>
2019-11-25 13:08:23 +0100

    Estructura básica del juego

---
+++ Juego.js
@@ -0,0 +1,13 @@
+import {nave} from "./Nave.js";
+import {marciano} from "./Marciano.js";
+import {disparo} from "./Disparo.js";
+
+export class juego{
+    constructor(){
+
+    }
+    
+    crearJuego(){
+
+    }
+}
---
+++ Nave.js
@@ -0,0 +1,5 @@
+export class nave extends objeto{
+    constructor(){
+        
+    }
+}
