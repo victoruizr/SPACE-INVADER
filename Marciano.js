96fac82dcc41f43b7f11f4df9f517a99bb5f7ba2
victoruizr <vmrr2204@gmail.com>
2019-11-25 14:08:41 +0100

    clase marciano modificado

--- Marciano.js
+++ Marciano.js
@@ -1,10 +1,29 @@
 export class marciano extends objeto{
-    constructor(){
+    constructor(x,y,velocidad,w,h,id){
+        super(this.x,this.y,this.velocidad,this.w,this.h);
+        this.id=id;
+        this.array=new Array(24);
+        this.crearMarciano();
+    }
+
+    crearMarciano(){
+        for(var i=0;i<this.array.length()-1;i++){
+            var fondo=document.getElementById("pantalla");
+            var marciano=document.createElementNS("http://www.w3.org/2000/svg","rect");
+            marciano.setAttribute("x",this.x);
+            marciano.setAttribute("y",this.y);
+            marciano.setAttribute("velocidad",this.velocidad);
+            marciano.setAttribute("width",this.w);
+            marciano.setAttribute("height",this.h);
+            fondo.appendChild(marciano);
+            this.array[i]=marciano;
+        }
+
 
     }
 
-    mover(){
-        super.mover();
-        this.y += this.v;
-    }
+
+
+
+    
 }
