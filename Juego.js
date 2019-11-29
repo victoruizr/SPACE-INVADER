977922482ce20780b92368f88f07aaf51093b17e
Víctor Manuel Ruiz Rodríguez <vmrr2204@gmail.com>
2019-11-29 14:15:46 +0100

    Modificacion de marciano y clase juego

--- Juego.js
+++ Juego.js
@@ -1,44 +1,78 @@
-import {nave} from "./Nave.js"
-import {marciano} from "./Marciano.js";
-import {disparo} from "./Disparo.js";
+import { nave } from "./Nave.js"
+import { marciano } from "./Marciano.js";
+import { disparo } from "./Disparo.js";
 
 /*------------------------------------VARIABLES---------------------------------*/
 
-var nav = new nave(325, 550, 4, 15, 10); // Se le pasa x, y, la velocidad, ancho y alto
-var disp = new disparo(325, 540, 2, 4, 10); // Se le pasa x, y, la velocidad, ancho y alto
+export class juego {
 
-/*------------------------------------FUNCIONES---------------------------------*/
-function dibujarYRellenarArray(){
-    var pantalla = document.getElementById("contenedor");
-    var y = pantalla.offsetHeight/8;
-    var marcianos=new Array(24);
-    var indice = 1; //Esta variable va a determinar el indice para cada nuevo Marciano
-    for(let i=0;i<3;i++){
-        var x = pantalla.offsetWidth/3;
-        for(let j=0;j<8;j++){
-            marcianos[indice] = new marciano(x,y,4,15,15); 
-            x = x+30; // Esta variable determina el espaciado entre marciano y marciano
-            if(j == 7){
-                y = y+30; //Esta variable determina el espaciado entre fila y fila
+    constructor() {
+        this.h = this.offsetHeight;
+        this.w = this.offsetWidth;
+        this.d = true;
+        this.crearObjetos();
+        this.iniciar();
+    }
+
+
+    dibujarYRellenarArray() {
+        var pantalla = document.getElementById("contenedor");
+        var y = pantalla.offsetHeight / 8;
+        var marcianos = new Array(24);
+        var indice = 1; //Esta variable va a determinar el indice para cada nuevo Marciano
+        for (let i = 0; i < 3; i++) {
+            var x = pantalla.offsetWidth / 3;
+            for (let j = 0; j < 8; j++) {
+                marcianos[indice] = new marciano(x, y, 4, 15, 15);
+                x = x + 30; // Esta variable determina el espaciado entre marciano y marciano
+                if (j == 7) {
+                    y = y + 30; //Esta variable determina el espaciado entre fila y fila
+                }
+                indice++; //Incremento del indice meter el siguiente Marciano
             }
-            indice++; //Incremento del indice meter el siguiente Marciano
         }
     }
+
+    controlMovimiento(e) {
+        if (e.keyCode == 87) {
+            this.d = true;
+            if (this.disp == undefined) {
+                this.disp = new disparo(325, 540, 2, 4, 10);
+                console.log("w");
+            } else {
+                this.d = false;
+            }
+        }
+    }
+
+
+
+
+    crearObjetos() {
+        var nav = new nave(325, 550, 4, 15, 10); // Se le pasa x, y, la velocidad, ancho y alto
+        // Se le pasa x, y, la velocidad, ancho y alto
+        this.dibujarYRellenarArray();
+    }
+
+
+    iniciar() {
+        document.addEventListener("keydown", (e) => { this.controlMovimiento(e) });
+        setInterval(() => 
+            {
+            if (this.disp != undefined) {
+                this.disp.moverDisparo();
+            }
+            if((this.disp!=undefined)&&(this.disp.obtenerY()+this.disp.obtenerH()<=0))
+            {
+                this.disp=undefined;
+            }
+        }, 10);
+    }
+
+
+
+
 }
 
-    function moverNave(){
-        nav.moverNave();
-    }
-
-    function disparar(){
-        disp.moverDisparo();
-    }
 
 
-/*-----------------------------------EJECUCION----------------------------------*/
-dibujarYRellenarArray();
-setInterval(() => { //Se llama a la funcion mover cada 30 centesimas de segundo
-    disparar();
-    moverNave();
-}, 30);
-/* crearPantalla(); */
--- Nave.js
+++ Nave.js
@@ -1,19 +1,20 @@
-export class nave{
-    constructor(x, y, v, w, h){
-        this.x=x;
-        this.y=y;
-        this.v=v;
-        this.w=w;
-        this.h=h;
+export class nave {
+    constructor(x, y, v, w, h) {
+        this.x = x;
+        this.y = y;
+        this.v = v;
+        this.w = w;
+        this.h = h;
         this.pantalla = document.getElementById("pantalla");
         this.contenedor = document.getElementById("contenedor")
-        this.wp = this.contenedor.offsetWidth ;
+        this.wp = this.contenedor.offsetWidth;
         this.hp = this.contenedor.offsetHeight;
+        this.moviNave = true;
         this.crearNave();
-}
+    }
 
-    crearNave(){
-        this.nave = document.createElementNS("http://www.w3.org/2000/svg","rect");
+    crearNave() {
+        this.nave = document.createElementNS("http://www.w3.org/2000/svg", "rect");
         this.nave.setAttribute("x", this.x);
         this.nave.setAttribute("y", this.y);
         this.nave.setAttribute("width", this.w);
@@ -22,22 +23,26 @@ export class nave{
         this.pantalla.appendChild(this.nave);
     }
 
-   moverNave(){
+
+
+
+    moverNaveDerecha() {
         /*MOVER EN EL EJE X*/
+        
         this.x += this.v;
         this.nave.setAttribute("x", this.x);
 
-        if(this.x+this.w > this.wp){
+        /* if (this.x + this.w > this.wp) {
             this.v = -this.v;
             this.x += this.v;
             this.nave.setAttribute("x", this.x);
-            
+
         }
-        
-        if(this.x < 0){
+
+        if (this.x < 0) {
             this.v = -this.v;
             this.x += this.v;
             this.nave.setAttribute("x", this.x);
-        }
+        }  */
     }
 }
